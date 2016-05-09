eb1c52c4cea54c33147e7f9328cc393e9d81c63d
kcates81 <kriscates81@gmail.com>
2016-05-09 11:23:12 -0500

    initial commit

---
+++ group_by_exercise.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT title
+FROM titles
+ORDER BY title ASC;
+
+SELECT DISTINCT first_name, last_name 
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name LIKE '%e';
+    
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
