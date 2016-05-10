c144710b16aeda6bd17992edc38b302862757c80
kcates81 <kriscates81@gmail.com>
2016-05-10 10:02:46 -0500

    added records to the table

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,8 +1,17 @@
 USE codeup_test_db;
 
-CREATE TABLE sales;
+INSERT INTO albums (id, artist, name, release_date, genre)
 
-INSERT INTO sales (artist_name)
-
-VALUES ();
+VALUES ('1', 'Shania Twain', 'Come on Over', '1997', 'country')
+('2', 'Led Zepplin', 'Led Zepplin IV', '1971', 'hard rock')
+('3', 'Michael Jackson', 'Bad', '1987', 'pop')
+('4', 'Alanis Morissette', 'Jagged Little Pill', '1995', 'alternative rock')
+('5', 'Celine Dion', 'Falling Into You', '1996', 'pop')
+('6', 'The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', '1967', 'rock')
+('7', 'Eagles', 'Hotel California', '1976', 'rock')
+('8', 'Mariah Carey', 'Music Box', '1993', 'pop')
+('9', 'Michael Jackson', 'Dangerous', '1991', 'rock')
+('10', 'Various Artists', 'Dirty Dancing', '1987', 'rock')
+('11', 'Celine Dion', 'Let''s Talk About Love', '1997', 'pop')
+('12', 'The Beatles', '1', '2000', 'rock');
 
