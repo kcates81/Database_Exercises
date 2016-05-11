f51b8d02ace5db2d0cc24d05c87c9c74c908e0cf
kcates81 <kriscates81@gmail.com>
2016-05-11 16:01:07 -0500

    initial commit

---
+++ extra_join_exercises.sql
@@ -0,0 +1,36 @@
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager AS dm
+  ON dm.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dm.dept_no;
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager AS dm
+  ON dm.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dm.dept_no
+WHERE e.gender = 'F'
+AND dm.to_date = '9999-01-01';
+
+SELECT DISTINCT t.title, COUNT(*)
+FROM employees as e
+JOIN dept_emp as de
+  ON de.emp_no = e.emp_no
+JOIN departments as d
+  ON d.dept_no = de.dept_no
+JOIN titles as t
+    ON t.emp_no =  de.emp_no 
+WHERE de.to_date > NOW() AND de.dept_no = 'd009' AND t.to_date > NOW()
+GROUP BY t.title;
+
+SELECT DISTINCT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, s.salary
+FROM employees AS e
+JOIN dept_manager AS dm
+  ON dm.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dm.dept_no
+JOIN salaries AS s
+    ON s.emp_no = dm.emp_no
+WHERE dm.emp_no = e.emp_no AND dm.to_date > NOW() AND s.to_date > NOW();
