edc7abc6703b9bef164fd6a3310966d5ac46a747
kcates81 <kriscates81@gmail.com>
2016-05-12 16:35:41 -0500

    added a SQL query that lists all of the departments with female managers

--- extra_join_exercises.sql
+++ extra_join_exercises.sql
@@ -34,3 +34,12 @@ JOIN departments AS d
 JOIN salaries AS s
     ON s.emp_no = dm.emp_no
 WHERE dm.emp_no = e.emp_no AND dm.to_date > NOW() AND s.to_date > NOW();
+
+SELECT d.dept_no, d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, e.gender
+FROM employees AS e
+JOIN dept_manager AS dm
+  ON dm.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dm.dept_no
+WHERE e.gender = 'F'
+AND dm.to_date = '9999-01-01';
